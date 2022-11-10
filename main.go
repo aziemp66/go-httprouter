61a00aea79abaef15d22ba02ab038e0a03ff192f
Azie Melza Pratama <aziemp66@gmail.com>
2022-11-10 09:08:18 +0700

    Router Pattern

--- main.go
+++ main.go
@@ -17,6 +17,17 @@ func main() {
 		fmt.Fprintf(w, "Products Id : %s", p.ByName("id"))
 	})
 
+	router.GET(
+		"/products/:id/items/:itemsId",
+		func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+			fmt.Fprintf(w, "Product Id : %s\nItems Id : %s", p.ByName("id"), p.ByName("itemsId"))
+		},
+	)
+
+	router.GET("/images/*image", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprintf(w, "Image Path : %s", p.ByName("image"))
+	})
+
 	server := http.Server{
 		Addr:    ":3000",
 		Handler: router,
