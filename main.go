effd2140b0005ac0d74442ad5bbaa22c60ace0ac
Azie Melza Pratama <aziemp66@gmail.com>
2022-11-10 09:02:13 +0700

    Params

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ func main() {
 		fmt.Fprint(w, "Hello Get")
 	})
 
+	router.GET("/products/:id", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprintf(w, "Products Id : %s", p.ByName("id"))
+	})
+
 	server := http.Server{
 		Addr:    ":3000",
 		Handler: router,
